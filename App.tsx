d266c9cbf2e62f26267a1da241464edcd8826aec
GabrielEagle <dialixkat@gmail.com>
2023-03-03 19:44:42 -0300

    input working, route line done

--- App.tsx
+++ App.tsx
@@ -1,27 +1,11 @@
-import { Text, StyleSheet, View } from 'react-native'
 import React, { Component } from 'react'
-import MapView from 'react-native-maps'
+import Map from './components/Map'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        {/* <Text>index</Text> */}
-
-        <MapView 
-            style={{ flex: 1 }}
-            region={{
-                latitude: -46.4226215,
-                longitude: -23.7079218,
-                latitudeDelta: 0.0143,
-                longitudeDelta: 0.0134,
-            }}
-            showsUserLocation
-            loadingEnabled
-        />
-      </View>
+      <Map />
     )
   }
 }
 
-const styles = StyleSheet.create({})
